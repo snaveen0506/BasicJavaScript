ce4477b069a7e21978578ad4f57a2261c81d338f
S <2164026@cognizant.com>
2024-07-18 12:08:33 +0530

    Changes in assessments are commited

--- Assessments/Assessment_JS/script.js
+++ Assessments/Assessment_JS/script.js
@@ -1,58 +1,3 @@
-/*Arrays:
-// Customer object
-let customers = [
-   {
-      'id': 001,
-      'f_name': 'Abby',
-      'l_name': 'Thomas',
-      'gender': 'M',
-      'married': true,
-      'age': 32,
-      'expense': 500,
-      'purchased': ['Shampoo', 'Toys', 'Book']
-   },
-   {
-      'id': 002,
-      'f_name': 'Jerry',
-      'l_name': 'Tom',
-      'gender': 'M',
-      'married': true,
-      'age': 64,
-      'expense': 100,
-      'purchased': ['Stick', 'Blade']
-   },
-   {
-      'id': 003,
-      'f_name': 'Dianna',
-      'l_name': 'Cherry',
-      'gender': 'F',
-      'married': true,
-      'age': 22,
-      'expense': 1500,
-      'purchased': ['Lipstik', 'Nail Polish', 'Bag', 'Book']
-   },
-   {
-      'id': 004,
-      'f_name': 'Dev',
-      'l_name': 'Currian',
-      'gender': 'M',
-      'married': true,
-      'age': 82,
-      'expense': 90,
-      'purchased': ['Book']
-   },
-   {
-      'id': 005,
-      'f_name': 'Maria',
-      'l_name': 'Gomes',
-      'gender': 'F',
-      'married': false,
-      'age': 7,
-      'expense': 300,
-      'purchased': ['Toys']
-   }
-];*/
-
 "use script";
 
 let customers = [
@@ -194,8 +139,16 @@ function forEachElement(arr, callback) {
   return a;
 }
 
-let ans = forEachElement(ages, call);
-console.log(ans);
+console.log(forEachElement(ages, call));
+
+//another way
+
+/*function callback(a) {
+  return a * 2;
+}
+
+let answer = ages.map(callback);
+console.log(answer);*/
 
 //Question 5
 
@@ -209,3 +162,27 @@ console.log(ans);
 //  the callback function will have message and url as argument,
 // if error is null then print error occured {message} {url}
 // else print the response is successfull the url is {url} and message is {message}
+
+function fetchData(url, callback) {
+  setTimeout(() => {
+    const error = Math.random() > 0.5; //create random numbers
+    const errMessage = "It' s not greater than 0.5";
+    const sucMessage = "It' s greater than 0.5";
+
+    if (error === true) {
+      callback(`Error occured is '${errMessage}' and URL is '${url}'`);
+    } else {
+      callback(
+        `The response is successful the url is '${url}' and the message is '${sucMessage}'`
+      );
+    }
+  }, 3000); //print result after 3 sec
+}
+
+function callBack(res) {
+  if (res) {
+    console.log(res); //res is 174 and 177 messages
+  }
+}
+
+fetchData("https://www.demo.com/", callBack);
